7251db2a30df469b3dab351b24cb17bba0ac2390
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2014-02-12 20:15:36 +0200

    Initial Commit

---
+++ Pastepasswd/PPIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PPIAppDelegate.h
+//  Pastepasswd
+//
+//  Created by Spiros Gerokostas on 2/12/14.
+//  Copyright (c) 2014 Spiros Gerokostas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PPIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Pastepasswd/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Pastepasswd
+//
+//  Created by Spiros Gerokostas on 2/12/14.
+//  Copyright (c) 2014 Spiros Gerokostas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PPIAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PPIAppDelegate class]));
+    }
+}
